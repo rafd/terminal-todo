2875fbef6bd95f9b91489c0d05c8ab2f739f36fa
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-01-26 15:55:58 +0800

    Add tasks as part of init!

--- src/todo/core.clj
+++ src/todo/core.clj
@@ -1,8 +1,10 @@
 (ns todo.core
   (:require 
     [reterm.core :as r]
-    [todo.views :refer [app-view]]))
+    [todo.views :refer [app-view]]
+    [todo.transact :as tx]))
 
 (defn -main []
   (let [mode (get [:swing :text] 0)]
+    (tx/init!)
     (r/render mode [app-view])))
--- src/todo/transact.clj
+++ src/todo/transact.clj
@@ -1,3 +1,9 @@
 (ns todo.transact
   (:require
     [todo.state :refer [state]]))
+
+(defn init! []
+  (swap! state assoc :tasks
+         [{:description "task 1"}
+          {:description "task 2"}
+          {:description "task 3"}]))
