e97d97a0eedd3747a4c3150c33995a1eb98149ce
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-01-26 15:55:38 +0800

    Remove stale state from todo app

--- src/todo/state.clj
+++ src/todo/state.clj
@@ -1,12 +1,4 @@
 (ns todo.state)
 
 (def state 
-  (atom {:tasks [{:description "task 1"}
-                 {:description "task 2"}
-                 {:description "task 3"}]
-         :cursor {:x 0
-                  :y 0
-                  :value nil}
-         :screen {:width nil
-                  :height nil}
-         :run? true}))
+  (atom {:tasks []}))
