76ae03c60ab01053119046d49c48a62aabe0160e
Rafal Dittwald <rafal.dittwald@gmail.com>
2018-01-26 16:22:31 +0800

    Generate random tasks

--- src/todo/transact.clj
+++ src/todo/transact.clj
@@ -1,12 +1,21 @@
 (ns todo.transact
   (:require
+    [clojure.string :as string]
     [todo.state :refer [state]]))
+
+(defn- random-word []
+  (rand-nth ["hello" "world" "foo" "bar" "baz"]))
+ 
+(defn- random-text [word-count]
+  (->> (repeatedly random-word)
+       (take word-count)
+       (string/join " ")))
+
+(defn- generate-task []
+  {:description (random-text 5)
+   :tag (random-text 1)})
 
 (defn init! []
   (swap! state assoc :tasks
-         [{:description "task 1"
-           :tag "tag 1"}
-          {:description "task 2"
-           :tag "tag 2"}
-          {:description "task 3"
-           :tag "tag 3"}]))
+         (->> (repeatedly generate-task)
+              (take 50))))
